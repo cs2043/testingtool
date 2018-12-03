e9556fd32a61edb8de53204daa276790de6994de
i7ec <i7ec@GE107M01.cs.unb.ca>
2018-12-03 01:13:07 -0400

    first change

--- TestProject/mytest/Junit.java
+++ TestProject/mytest/Junit.java
@@ -2,6 +2,7 @@ package mytest;
 
 public class Junit {
 public String concatenate(String one, String two) {
+	System.out.println("testing");
 	return one +two;
 }
 public int multiply (int a, int b) {
